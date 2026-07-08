06822bc929d59094a55b228544f9db2330fcb44a
Michal-Babins <michal.babinski@theiagen.com>
2026-07-08 20:31:20 +0000

    update path

--- test_run.sh
+++ test_run.sh
@@ -1,5 +1,7 @@
 uv run --project . theiavalidate validate \
-    tests/phb/theiaprok/theiaprok_illumina_pe_v4-2-0.tsv \
-    tests/phb/theiaprok/theiaprok_illumina_pe_v4-1-0.tsv \
-    --config validation-criteria-v4-2-0-theiaprok-illumina-pe.yaml \
+    tests/phb/theiaprok/pe/theiaprok_illumina_pe_v4-2-0.tsv \
+    tests/phb/theiaprok/pe/theiaprok_illumina_pe_v4-1-0.tsv \
+    --preset theiaprok_pe \
+    --key1 entity:theiaprok_illumina_pe_v4-2-0_id \
+    --key2 entity:theiaprok_illumina_pe_v4-1-0_id \
     --outdir out --prefix theiaprok_pe_4-2-0_v4-1-0
