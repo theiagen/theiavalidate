69ce509ebb5bf7a1048f09f48e80e72175905998
Michal-Babins <michal.babinski@theiagen.com>
2026-07-16 16:23:12 +0000

    bye bye

--- tests/test_config_smoke.py
+++
@@ -1,8 +0,0 @@
-from theiavalidate import Config, TypeSpec
-
-def test_typespec():
-    assert str(TypeSpec.parse("set[float]")) == "set[float]"
-
-def test_config_smoke():
-    cfg = Config.from_dict({"key": "s", "columns": {"c": {"method": "exact", "type": "str"}}})
-    assert cfg.key == "s"
